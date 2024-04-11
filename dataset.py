ad3cac40dceb696e67c64b2ea700ddb45311646e
Priyanshu546 <146194215+Priyanshu546@users.noreply.github.com>
2024-04-11 10:46:30 +0530

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,20 @@
+dataset = pd.read_csv("UberDataset.csv")
+dataset.head()
+dataset.shape
+dataset.info()
+dataset['PURPOSE'].fillna("NOT", inplace=True)
+dataset['START_DATE'] = pd.to_datetime(dataset['START_DATE'], 
+									errors='coerce')
+dataset['END_DATE'] = pd.to_datetime(dataset['END_DATE'], 
+									errors='coerce')
+from datetime import datetime
+
+dataset['date'] = pd.DatetimeIndex(dataset['START_DATE']).date
+dataset['time'] = pd.DatetimeIndex(dataset['START_DATE']).hour
+
+#changing into categories of day and night
+dataset['day-night'] = pd.cut(x=dataset['time'],
+							bins = [0,10,15,19,24],
+							labels = ['Morning','Afternoon','Evening','Night'])
+dataset.dropna(inplace=True)
+dataset.drop_duplicates(inplace=True)
